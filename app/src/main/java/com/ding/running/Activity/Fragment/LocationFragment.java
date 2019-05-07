1338657d00c32df6db4337908c1609377efb4202
Leoren <3290183219@qq.com>
2019-05-07 19:43:04 +0800

    second commit

--- app/src/main/java/com/ding/running/Activity/Fragment/LocationFragment.java
+++ app/src/main/java/com/ding/running/Activity/Fragment/LocationFragment.java
@@ -1,6 +1,36 @@
 package com.ding.running.Activity.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.ding.running.MyViews.ToastUtil;
+import com.ding.running.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName LocationFragment
@@ -10,4 +40,155 @@ import android.support.v4.app.Fragment;
  * @Version v1.0
  */
 public class LocationFragment extends Fragment {
+
+    private MapView playItemMapView;  // mapview
+    private BaiduMap baiduMap;    //baiduMap 用来管理mapview
+    LocationClient locationClient;
+    private LocationClientOption option;
+    public MyLocationListener myLocationListener = new MyLocationListener();
+
+    private TextView positionText;
+
+    private boolean isFirstLocate = true;
+
+    private View rootView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_location, container, false);
+        requestPermission();
+        playItemMapView = rootView.findViewById(R.id.my_location_map);
+        positionText = rootView.findViewById(R.id.position_text);
+        baiduMap = playItemMapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
+        locationClient = new LocationClient(getActivity());
+        locationClient.registerLocationListener(myLocationListener);
+        option = new LocationClientOption();
+        option.setOpenGps(true);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        locationClient.setLocOption(option);
+        locationClient.start();
+        return rootView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        initLocation();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+
+    private void requestPermission(){
+        List<String> premissionList = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            premissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            premissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+            premissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            premissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if(!premissionList.isEmpty()){
+            String[] permission = premissionList.toArray(new String[premissionList.size()]);
+            ActivityCompat.requestPermissions(getActivity(),  permission, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0){
+                    for(int result : grantResults){
+                        if(result != PackageManager.PERMISSION_GRANTED){
+                            ToastUtil.MakeToast("必须由此权限才能使用本程序");
+                            getActivity().finish();
+                            return;
+                        }
+                    }
+                }else {
+                    ToastUtil.MakeToast("发生未知错误");
+                    getActivity().finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        playItemMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        playItemMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        locationClient.stop();
+        baiduMap.setMyLocationEnabled(false);
+        playItemMapView.onDestroy();
+        playItemMapView = null;
+        super.onDestroy();
+
+    }
+
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || playItemMapView == null)
+                return;
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            baiduMap.setMyLocationData(locData);
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            // 设置地图中心点
+            MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory
+                    .newMapStatus(new MapStatus.Builder().target(latLng)
+                            .overlook(-15).build());
+            baiduMap.setMapStatus(mapStatusUpdate);
+            String country = location.getCountry();
+            String province = location.getProvince();
+            String city = location.getCity();
+            String street = location.getStreet();
+            StringBuilder sb = new StringBuilder();
+            sb.append(country).append(province).append(city).append(street);
+            positionText.setText(location.getAddrStr());
+            if (isFirstLocate) {
+                isFirstLocate = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+                baiduMap.animateMapStatus(update);
+                update = MapStatusUpdateFactory.zoomBy(12f);
+                baiduMap.animateMapStatus(update);
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/ding/running/Application/MyApplication.java
+++ app/src/main/java/com/ding/running/Application/MyApplication.java
@@ -2,6 +2,8 @@ package com.ding.running.Application;
 
 import android.content.Context;
 
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
 import com.ding.running.Utils.OkHttp.CommonOkHttpClient;
 
 import org.litepal.LitePalApplication;
@@ -19,6 +21,10 @@ public class MyApplication extends LitePalApplication {
         super.onCreate();
         context = this;
         CommonOkHttpClient.initClient();
+        SDKInitializer.initialize(this);
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
 
     }
 
